db97dad15cc202831c63350f1becf7d364b1a75b
santhoshjamesc <santoze49@gmail.com>
2026-02-27 10:54:56 +0000

    hash

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,7 @@ export default function App() {
           />
 
           <Route
-            path="#/services"
+            path="/services"
             element={
               <>
                 <SEO
